8d731e8d0c0b26a617bce65f24f72834ee01168f
vsc-github <batty.wayn3@gmail.com>
2018-11-03 01:40:16 +0530

    add new styles for posts

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -10,11 +10,42 @@ class Post extends React.Component {
 
     return (
       <div className="post">
-        <div className="post__meta">
-          <time className="post__meta-time" dateTime={moment(date).format('MMMM D, YYYY')}>
-            {moment(date).format('MMMM YYYY')}
+        <Link to={slug} className="post__left">
+          <div className="post__left__wrap">
+            <img
+              src="https://cdn.dribbble.com/users/823823/screenshots/2453764/dribbble_098_2x_1x.png"
+              alt=""/>
+          </div>
+        </Link>
+        <div className="post__right">
+          <h2 className="post__title">
+            <Link className="post__title-link" to={slug}>{title}</Link>
+          </h2>
+          <time className="post__meta-time" dateTime={moment(date)
+            .format('MMMM D, YYYY')}>
+            {moment(date)
+              .format('MMMM D, YYYY')}
           </time>
-          <span className="post__meta-divider" />
+          <p className="post__description">{description}</p>
+          <span className="post__category" key={categorySlug}>
+            <img src="https://image.flaticon.com/icons/svg/25/25628.svg" alt=""/>
+            <Link to={categorySlug} className="post__meta-category-link">
+               {category}
+            </Link>
+          </span>
+        </div>
+      </div>
+    );
+
+    return (
+      <div className="post">
+        <div className="post__meta">
+          <time className="post__meta-time" dateTime={moment(date)
+            .format('MMMM D, YYYY')}>
+            {moment(date)
+              .format('MMMM D, YYYY')}
+          </time>
+          <span className="post__meta-divider"/>
           <span className="post__meta-category" key={categorySlug}>
             <Link to={categorySlug} className="post__meta-category-link">
               {category}
