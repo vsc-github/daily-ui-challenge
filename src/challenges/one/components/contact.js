20375faadbb2616e92ad9c6d141ae92df7e39c55
vsc-github <batty.wayn3@gmail.com>
2018-11-05 16:05:39 +0530

    set up selected state for chat thread

---
+++ src/challenges/one/components/contact.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import './contact.css';
+
+class Contact extends React.Component {
+  render() {
+    const { contact, selected } = this.props;
+
+    return (
+      <div className="contact" style={selected ? {
+        backgroundColor: 'white',
+        boxShadow: '0 10px 20px 10px #f3eaed'
+      } : null}>
+        {!selected ? <div className="bar"></div> : null}
+        <br/>
+        <div className="avatar">
+          <img src="https://randomuser.me/api/portraits/women/63.jpg" alt=""/>
+        </div>
+        <div className="text">
+          <div className="name">Alex</div>
+          <div className="msg">Do you remember the name of movie we watch last sunday?</div>
+        </div>
+        <div className="stats">
+          <div className="time">18:06</div>
+          <div className="unread">1</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/challenges/one/one.js
+++ src/challenges/one/one.js
@@ -1,17 +1,68 @@
 import React from 'react';
+import Contact from './components/contact';
 
 import './one.css';
 
 class One extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      activeIndex: 2,
+      contacts : [
+        {
+          name: 'Vanessa',
+          img: '',
+          location: '',
+          thread: [
+            {
+              text: 'When should we meet? Maybe at 2-3pm or later?',
+              self: true,
+            },
+            {
+              text: "Let\'s meet at best pizza house. I haven't been there in a long time. I heard they just came ip with a new pizza.",
+              self: false,
+            },
+            {
+              text: 'That sounds like a good idea!',
+              self: true,
+            },
+            {
+              text: "How  about going to see a movie? Cinemax 26 on Broadway is showing Enchanted.",
+              self: false,
+            }
+          ]
+        }
+      ]
+    };
   }
 
   render() {
     return (<div className="base">
       <div className="chg">
-        ONEEEEEE
+        <img src="https://res.cloudinary.com/dzas6ep30/image/upload/v1541247085/dailyui/texture.jpg"
+             alt="" className="hero"/>
+        <div className="maincard">
+          <div className="nav"></div>
+          <div className="contacts">
+            <Contact />
+            <Contact selected />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
+          </div>
+          <div className="thread"></div>
+        </div>
       </div>
     </div>);
   }
