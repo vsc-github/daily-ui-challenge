d285bac75355bd2cf4886b382c92c4129f3d5ddb
vsc-github <batty.wayn3@gmail.com>
2018-11-05 23:12:17 +0530

    add message thread styles

--- src/challenges/one/one.js
+++ src/challenges/one/one.js
@@ -52,19 +52,66 @@ class One extends React.Component {
           <div className="nav"></div>
           <div className="contacts">
             <div className="search">
-              <img src="https://res.cloudinary.com/dzas6ep30/image/upload/v1541416229/dailyui/1/search.svg" alt=""/>
+              <img
+                src="https://res.cloudinary.com/dzas6ep30/image/upload/v1541416229/dailyui/1/search.svg"
+                alt=""/>
               <input type="text" placeholder="Search"/>
             </div>
             <div className="list">
-            {
-              'somanycontacts'.split('')
-                .map((contact, key) => <Contact setActiveIndexToFunc={this.setActiveIndexTo}
-                                                index={key}
-                                                selected={this.state.activeIndex === key}/>)
-            }
+              {
+                'somanycontacts'.split('')
+                  .map((contact, key) => <Contact setActiveIndexToFunc={this.setActiveIndexTo}
+                                                  index={key}
+                                                  selected={this.state.activeIndex === key}/>)
+              }
             </div>
           </div>
-          <div className="thread"></div>
+          <div className="thread">
+            <div className="top">
+              <img src="https://randomuser.me/api/portraits/women/63.jpg" alt=""/>
+              <div className="details">
+                <div className="name">Vanessa</div>
+                <div className="time">18:05 Los-Angeles</div>
+              </div>
+              <div className="state">
+                Online
+              </div>
+              <div className="bar"></div>
+            </div>
+            <div className="middle">
+              <div className="rope">
+                <div className="message">
+                  <div className="time">16:05</div>
+                  <div className="text">When should we meet? Maybe at 2-3pm or later?</div>
+                </div>
+                <div className="message me">
+                  <div className="time">16:08</div>
+                  <div className="text">Let's meet at best pizza house. I haven't been there in a
+                    long time. I heard they just came ip with a new pizza.
+                  </div>
+                </div>
+                <div className="message">
+                  <div className="time">16:15</div>
+                  <div className="text">When should we meet? Maybe at 2-3pm or later?</div>
+                </div>
+                <div className="message me">
+                  <div className="time">16:23</div>
+                  <div className="text">Let's meet at best pizza house. I haven't been there in a
+                    long time. I heard they just came ip with a new pizza.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bottom">
+              <input type="text" placeholder="your message"/>
+              <img
+                src="https://res.cloudinary.com/dzas6ep30/image/upload/v1541426892/dailyui/1/plus.svg"
+                alt="+" className="plus"/>
+              <img
+                src="https://res.cloudinary.com/dzas6ep30/image/upload/v1541426981/dailyui/1/message.svg"
+                alt="send" className="send"/>
+            </div>
+          </div>
         </div>
       </div>
     </div>);
